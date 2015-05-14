7f8e9885c6b3c2bb019fe46a26e0ff6a25509c62
Wilian Watanabe Nunes <wilian.android@gmail.com>
2015-05-14 15:32:47 -0300

    iniciando o curso

---
+++ module/Base/src/Base/Entity/AbstractEntity.php
@@ -0,0 +1,19 @@
+<?php
+namespace Base\Entity;
+
+use Zend\Stdlib\Hydrator\ClassMethods;
+
+abstract class AbstractEntity
+{
+    public function __contruct(Array $options = array())
+    {
+        $hydrator = new ClassMethods();
+        $hydrator->hydrate($options, $this);
+    }
+    
+    public function toArray()
+    {
+        $hydrator = new ClassMethods();
+        return $hydrator->extract($this);
+    }
+}
